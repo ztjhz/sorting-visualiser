2512c65c622cd8fcbd50ebd1282fd3d88ae394c9
ztjhz <59118459+ztjhz@users.noreply.github.com>
2021-06-10 16:12:42 +0800

    javascript formatting

--- index.js
+++ index.js
@@ -1,74 +1,82 @@
 /* remove loader icon and show the content when ready */
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#container').hidden = false;
-    document.querySelector('.loader').remove();
-})
+  document.querySelector('#container').hidden = false;
+  document.querySelector('.loader').remove();
+});
 
 /* For visualising speed */
 const speed_slider = document.querySelector('#speed');
 speed_slider.style.direction = 'rtl';
-speed_slider.oninput = () => sort_speed = parseInt(speed_slider.value);
+speed_slider.oninput = () => (sort_speed = parseInt(speed_slider.value));
 
 /* for size of the array */
 const size_slider = document.querySelector('#size');
 size_slider.oninput = () => {
-    len = parseInt(size_slider.value);
-    arrays = createArray(len);
-    arr = arrays[0];
-    state = arrays[1];
-    width = Math.floor(canvasW / len);
-}
-btns = []
+  len = parseInt(size_slider.value);
+  arrays = createArray(len);
+  arr = arrays[0];
+  state = arrays[1];
+  width = Math.floor(canvasW / len);
+};
+btns = [];
 const quick_sort_btn = document.querySelector('#quickSort');
 const bubble_sort_btn = document.querySelector('#bubbleSort');
 const insertion_sort_btn = document.querySelector('#insertionSort');
 const merge_sort_btn = document.querySelector('#mergeSort');
 const selection_sort_btn = document.querySelector('#selectionSort');
 
-
-btns.push({
-        btn: quick_sort_btn,
-        sort: () => quickSort(arr, 0, arr.length - 1)
-    }, {
-        btn: bubble_sort_btn,
-        sort: () => bubbleSort(arr)
-    }, {
-        btn: insertion_sort_btn,
-        sort: () => insertionSort(arr)
-    }, {
-       btn: merge_sort_btn,
-       sort: () => mergeSort(arr, 0, arr.length - 1)
-                   .then(() => state.fill(stateColor.sorted.value))
-    }, {
-        btn: selection_sort_btn,
-        sort: () => selectionSort(arr)
-    })
+btns.push(
+  {
+    btn: quick_sort_btn,
+    sort: () => quickSort(arr, 0, arr.length - 1)
+  },
+  {
+    btn: bubble_sort_btn,
+    sort: () => bubbleSort(arr)
+  },
+  {
+    btn: insertion_sort_btn,
+    sort: () => insertionSort(arr)
+  },
+  {
+    btn: merge_sort_btn,
+    sort: () =>
+      mergeSort(arr, 0, arr.length - 1).then(() =>
+        state.fill(stateColor.sorted.value)
+      )
+  },
+  {
+    btn: selection_sort_btn,
+    sort: () => selectionSort(arr)
+  }
+);
 
 btns.forEach(btn => {
-    btn.btn.onclick = () => {
-        state.fill(stateColor.unsorted.value) /* reset the color back to unsorted */
-        btn.sort()
-        .then(() => enableInputs());
-        disableInputs();
-    }
+  btn.btn.onclick = () => {
+    state.fill(
+      stateColor.unsorted.value
+    ); /* reset the color back to unsorted */
+    btn.sort().then(() => enableInputs());
+    disableInputs();
+  };
 });
 
 function createArray(len) {
-    let arr = new Array(len);
-    let state = new Array(len);
-    for (let i = 0; i < len; i++) {
-        arr[i] = Math.floor(Math.random() * (canvasH / 2)) + 1;
-        state[i] = stateColor.unsorted.value;
-    }
-    return [arr, state];
+  let arr = new Array(len);
+  let state = new Array(len);
+  for (let i = 0; i < len; i++) {
+    arr[i] = Math.floor(Math.random() * (canvasH / 2)) + 1;
+    state[i] = stateColor.unsorted.value;
+  }
+  return [arr, state];
 }
 
 function enableInputs() {
-    btns.forEach(btn => btn.btn.disabled = false);
-    size_slider.disabled = false;
+  btns.forEach(btn => (btn.btn.disabled = false));
+  size_slider.disabled = false;
 }
 
 function disableInputs() {
-    btns.forEach(btn => btn.btn.disabled = true);
-    size_slider.disabled = true;
+  btns.forEach(btn => (btn.btn.disabled = true));
+  size_slider.disabled = true;
 }
--- sketch.js
+++ sketch.js
@@ -2,54 +2,55 @@
 let canvasW = 500;
 let canvasH = 200;
 
-let sort_speed = 50 /* determines the speed of the sorting */
+let sort_speed = 50; /* determines the speed of the sorting */
 let len = 100; /* Length of the array to be sorted */
-let width = Math.floor(canvasW / len); /* width of the blocks of array element shown on canvas */
+let width = Math.floor(
+  canvasW / len
+); /* width of the blocks of array element shown on canvas */
 
 let arr;
 let state; /* state of the array elements: -2 comparing element, -1 unsorted, 0 pivot element, 1 sorted */
 const stateColor = {
-    sorted: {value: 1, color: '#00FF00'}, /* lime */
-    unsorted: {value: 0, color: '#FFFFFF'}, /* white */
-    compare: {value: -1, color: '#F0E68C'}, /* khaki */
-    pivot: {value: -2, color: '#DC143C'}, /* crimson */
-}
-let sortedColor = '#00FF00'; 
-let unsortedColor = '#FFFFFF' 
-let partitionColor = '#DC143C' 
-let comparedColor = '#F0E68C'; 
-
+  sorted: { value: 1, color: '#00FF00' } /* lime */,
+  unsorted: { value: 0, color: '#FFFFFF' } /* white */,
+  compare: { value: -1, color: '#F0E68C' } /* khaki */,
+  pivot: { value: -2, color: '#DC143C' } /* crimson */
+};
+let sortedColor = '#00FF00';
+let unsortedColor = '#FFFFFF';
+let partitionColor = '#DC143C';
+let comparedColor = '#F0E68C';
 
 function setup() {
-    if (canvasW > windowWidth) {
-        canvasW = windowWidth;
-    }
-    let canvas = createCanvas(canvasW, canvasH);
-    canvas.parent('canvas-container');
-    const arrays = createArray(len);
-    arr = arrays[0];
-    state = arrays[1];
-    //quickSort(arr, 0, arr.length - 1);
+  if (canvasW > windowWidth) {
+    canvasW = windowWidth;
+  }
+  let canvas = createCanvas(canvasW, canvasH);
+  canvas.parent('canvas-container');
+  const arrays = createArray(len);
+  arr = arrays[0];
+  state = arrays[1];
+  //quickSort(arr, 0, arr.length - 1);
 }
 
 function draw() {
-    background(50);
-    noStroke();
-    for (let i = 0; i < arr.length; i++) {
-        switch (state[i]) {
-            case stateColor.compare.value: /* being compared */
-                fill(stateColor.compare.color);
-                break;
-            case stateColor.unsorted.value: /* unsorted */
-                fill(stateColor.unsorted.color);
-                break;
-            case stateColor.pivot.value: /* partition */
-                fill(stateColor.pivot.color);
-                break;
-            case stateColor.sorted.value: /* sorted */
-                fill(stateColor.sorted.color);
-                break;
-        }
-        rect(i * width, canvasH/10 * 9 - arr[i], width, arr[i]);
+  background(50);
+  noStroke();
+  for (let i = 0; i < arr.length; i++) {
+    switch (state[i]) {
+      case stateColor.compare.value /* being compared */:
+        fill(stateColor.compare.color);
+        break;
+      case stateColor.unsorted.value /* unsorted */:
+        fill(stateColor.unsorted.color);
+        break;
+      case stateColor.pivot.value /* partition */:
+        fill(stateColor.pivot.color);
+        break;
+      case stateColor.sorted.value /* sorted */:
+        fill(stateColor.sorted.color);
+        break;
     }
+    rect(i * width, (canvasH / 10) * 9 - arr[i], width, arr[i]);
+  }
 }
